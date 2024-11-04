29b9079ffc8c67e2e819b2c51b44db7ea0e1ba40
kaushik.sst@gmail.com <kaushik.sst@gmail.com>
2024-11-04 23:39:20 +0000

    Modifed index.html to suit https configuration

--- src/main/java/org/sai/qod/controller/QodController.java
+++ src/main/java/org/sai/qod/controller/QodController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/quotes")
-@CrossOrigin(origins = "http://")
+@CrossOrigin(origins = "*")
 public class QodController {
 
     @Autowired
