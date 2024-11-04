125671963d001ffcacd79d30af56cccddb0e2c4e
kaushik.sst@gmail.com <kaushik.sst@gmail.com>
2024-11-04 19:51:37 +0000

    Modified CORS

--- src/main/java/org/sai/qod/controller/QodController.java
+++ src/main/java/org/sai/qod/controller/QodController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/quotes")
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 public class QodController {
 
     @Autowired
